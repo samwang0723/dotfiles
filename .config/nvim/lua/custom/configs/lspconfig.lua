2761fff61ec998d1ab850a5f9b55fbf2a15e5377
Sam Wang <sam.wang.0723@gmail.com>
2024-11-20 09:18:12 +0800

    fix: update lspconfig to use ts_ls

--- .config/nvim/lua/custom/configs/lspconfig.lua
+++ .config/nvim/lua/custom/configs/lspconfig.lua
@@ -1,134 +1,134 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
-local util = require "lspconfig/util"
+local lspconfig = require("lspconfig")
+local util = require("lspconfig/util")
 
-lspconfig.gopls.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  cmd = { "gopls", "-v", "-rpc.trace", "serve", "--debug=localhost:6060" },
-  filetypes = { "go", "gomod", "gowork", "gotmpl" },
-  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-  settings = {
-    gopls = {
-      completeUnimported = true,
-      staticcheck = false,
-      usePlaceholders = true,
-      semanticTokens = true,
-      codelenses = {
-        generate = true,
-        test = true,
-      },
-      matcher = "fuzzy",
-      symbolMatcher = "fuzzy",
-      analyses = {
-        printf = true,
-        fillreturns = true,
-        nonewvars = true,
-        undeclaredname = true,
-        unusedparams = true,
-        unreachable = true,
-        fieldalignment = true,
-        ifaceassert = true,
-        nilness = true,
-        shadow = true,
-        unusedwrite = true,
-        fillstruct = true,
-      },
-      annotations = {
-        escape = true,
-        inline = true,
-        bounds = true,
-      },
-      deepCompletion = true,
-      verboseOutput = true,
-      gofumpt = true,
-      directoryFilters = {
-        "-node_modules",
-        "-third_party",
-      },
-    },
-  },
-}
+lspconfig.gopls.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	cmd = { "gopls", "-v", "-rpc.trace", "serve", "--debug=localhost:6060" },
+	filetypes = { "go", "gomod", "gowork", "gotmpl" },
+	root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+	settings = {
+		gopls = {
+			completeUnimported = true,
+			staticcheck = false,
+			usePlaceholders = true,
+			semanticTokens = true,
+			codelenses = {
+				generate = true,
+				test = true,
+			},
+			matcher = "fuzzy",
+			symbolMatcher = "fuzzy",
+			analyses = {
+				printf = true,
+				fillreturns = true,
+				nonewvars = true,
+				undeclaredname = true,
+				unusedparams = true,
+				unreachable = true,
+				fieldalignment = true,
+				ifaceassert = true,
+				nilness = true,
+				shadow = true,
+				unusedwrite = true,
+				fillstruct = true,
+			},
+			annotations = {
+				escape = true,
+				inline = true,
+				bounds = true,
+			},
+			deepCompletion = true,
+			verboseOutput = true,
+			gofumpt = true,
+			directoryFilters = {
+				"-node_modules",
+				"-third_party",
+			},
+		},
+	},
+})
 
-lspconfig.golangci_lint_ls.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  flags = {
-    debounce_text_changes = 150,
-  },
-}
+lspconfig.golangci_lint_ls.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	flags = {
+		debounce_text_changes = 150,
+	},
+})
 
 -- Javascript & Typescript LSP
-lspconfig.tsserver.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  flags = {
-    debounce_text_changes = 150,
-  },
-}
+lspconfig.ts_ls.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	flags = {
+		debounce_text_changes = 150,
+	},
+})
 
 -- HTML/CSS LSP
 -- npm i -g vscode-langservers-extracted
-lspconfig.html.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  flags = {
-    debounce_text_changes = 150,
-  },
-}
+lspconfig.html.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	flags = {
+		debounce_text_changes = 150,
+	},
+})
 
 -- npm i -g vscode-langservers-extracted
-lspconfig.cssls.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  flags = {
-    debounce_text_changes = 150,
-  },
-}
+lspconfig.cssls.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	flags = {
+		debounce_text_changes = 150,
+	},
+})
 
 -- npm i -g vscode-langservers-extracted
-lspconfig.jsonls.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-}
+lspconfig.jsonls.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
 
 -- Python
-lspconfig.pyright.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = { "python" },
-}
+lspconfig.pyright.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	filetypes = { "python" },
+})
 
 -- Solargraph
-lspconfig.solargraph.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  flags = {
-    debounce_text_changes = 150,
-  },
-  root_dir = function(fname)
-    local root = fname:match ".*/github.com/cdc-internal/.-/"
-    return root ~= nil and root or util.root_pattern(".git", "Gemfile")(fname)
-  end,
-  commandPath = "/Users/samwang/.asdf/shims/solargraph",
-  useBundler = false,
-  diagnostics = true,
-  completion = true,
-  autoformat = true,
-  diagnostic = true,
-  folding = true,
-  references = true,
-  rename = true,
-  symbols = true,
-}
+lspconfig.solargraph.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	flags = {
+		debounce_text_changes = 150,
+	},
+	root_dir = function(fname)
+		local root = fname:match(".*/github.com/cdc-internal/.-/")
+		return root ~= nil and root or util.root_pattern(".git", "Gemfile")(fname)
+	end,
+	commandPath = "/Users/samwang/.asdf/shims/solargraph",
+	useBundler = false,
+	diagnostics = true,
+	completion = true,
+	autoformat = true,
+	diagnostic = true,
+	folding = true,
+	references = true,
+	rename = true,
+	symbols = true,
+})
 
 -- General LSP loading
 local f_status_ok, fidget = pcall(require, "fidget")
 if not f_status_ok then
-  vim.notify "fidget: cannot be found!"
-  return
+	vim.notify("fidget: cannot be found!")
+	return
 end
 
-fidget.setup {}
+fidget.setup({})
