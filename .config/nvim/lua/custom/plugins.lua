7e678f566ac40080a1388c9ab01b5e1fea768c84
Sam Wang <sam.wang.0723@gmail.com>
2025-01-07 14:29:36 +0800

    fix: update moody branch sync and remove CopilotChat

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -222,28 +222,16 @@ local plugins = {
     },
   },
 
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "canary",
-    dependencies = {
-      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
-      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-    },
-    opts = {
-      debug = true, -- Enable debugging
-      -- See Configuration section for rest
-    },
-    lazy = false,
-    -- See Commands section for default commands if you want to lazy load on them
-  },
-
   -- Moody for mode cursorline
   {
     "svampkorg/moody.nvim",
     event = { "ModeChanged", "BufWinEnter", "WinEnter" },
     dependencies = {
-      -- or wherever you setup your HL groups :)
+      -- or whatever "colorscheme" you use to setup your HL groups :)
+      -- Colours can also be set within setup, in which case this is redundant.
       "catppuccin/nvim",
+      -- for seeing Moody's take on folds
+      "kevinhwang91/nvim-ufo",
     },
     opts = {
       -- you can set different blend values for your different modes.
@@ -263,7 +251,7 @@ local plugins = {
       -- there are two ways to define colours for the different modes.
       -- one way is to define theme here in colors. Another way is to
       -- set them up with highlight groups. Any highlight group set takes
-      -- precedence over any colors defined here.
+      -- precedence over any colours defined here.
       colors = {
         normal = "#00BFFF",
         insert = "#70CF67",
@@ -278,8 +266,48 @@ local plugins = {
       -- disable filetypes here. Add for example "TelescopePrompt" to
       -- not have any coloured cursorline for the telescope prompt.
       disabled_filetypes = { "TelescopePrompt" },
+      -- disabled buftypes here
+      disabled_buftypes = {},
       -- you can turn on or off bold characters for the line numbers
       bold_nr = true,
+      -- you can turn on and off a feature which shows a little icon and
+      -- registry number at the end of the CursorLine, for when you are
+      -- recording a macro! Default is false.
+      recording = {
+        enabled = false,
+        icon = "󰑋",
+        -- you can set some text to surround the recording registry char with
+        -- or just set one to empty to maybe have just one letter, an arrow
+        -- perhaps! For example recording to q, you could have! "󰑋    q" :D
+        pre_registry_text = "[",
+        post_registry_text = "]",
+        -- if you have some other plugin showing up on the right you can pad
+        -- the recording indicator on the right side, to shift it to the left.
+        -- For example if you use Satellite you have to shift recording to the left.
+        -- using right padding.
+        right_padding = 2,
+      },
+      -- extend the cursorline to cover line numbers.
+      extend_to_linenr = true,
+      -- extend_to_linenr_visual enables moodys built in statuscolumn to cover linenr in
+      -- visual selection to show the visual range of the selection highlighted by
+      -- Visual hl group.
+      extend_to_linenr_visual = false,
+      -- setting reduce_cursorline to true will reduce the moodys cursorline to.. nothing!
+      -- It will make the cursorline have the default value of whatever cursorline has in the
+      -- default "hightlight namespace", 0. TLDR: no moody change in cursorline with ModeChanged.
+      reduce_cursorline = false,
+      -- fold_options.enabled also enables the built in SignColumn in moody. These folds takes a bit of a different
+      -- approach to showing folds and their range. Try it out and see if you like it :) If not you can use
+      -- the SignColumn as is, with extend_to_linenr. It will then only show diagnostic signs and linenr.
+      -- This requires nvim-ufo, and will possibly break your current custom SignColumn (if you have any)
+      fold_options = {
+        enabled = false,
+        -- these are two colors you can specifiy which will be used to generate a gradient
+        -- with one step for each fold level, specified by vim.o.foldnestmax
+        start_color = "#C1C1C1",
+        end_color = "#2F2F2F",
+      },
     },
   },
 
